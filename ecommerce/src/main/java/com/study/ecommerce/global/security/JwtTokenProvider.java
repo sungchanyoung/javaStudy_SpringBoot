dd56cf248ca9f3f815bd6bc0487bbeb050ab329a
SungChanYoung <zkzk7290@naver.com>
2025-05-24 21:06:46 +0900

    error 처리

---
+++ ecommerce/src/main/java/com/study/ecommerce/global/security/JwtTokenProvider.java
@@ -0,0 +1,52 @@
+package com.study.ecommerce.global.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.hibernate.annotations.Comment;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class JwtTokenProvider {
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    @Value("${token-validity-in-milliseconds}")
+    private long tokenValidityInMilliseconds;
+
+    private Key key;
+
+    private final UserDetailsService userDetailsService;
+
+    @PostConstruct
+    protected void init() {
+        this.key = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public String createToken(String username, List<String> roles) {
+       Claims claims = Jwts.claims().setSubject(username);
+       claims.put("roles", roles);
+
+       Date now = new Date();
+       Date validity = new Date(now.getTime() + tokenValidityInMilliseconds);
+
+       return Jwts.builder()
+               .setClaims(claims)
+               .setIssuedAt(now)
+               .setExpiration(validity)
+               .signWith(key, SignatureAlgorithm.HS256)
+               .compact();
+    }
+}
